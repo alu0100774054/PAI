68e04c105ccde0060aa0c1664a6d38b4e4304cd6
alu0100774054 <alu0100774054@ull.edu.es>
2016-03-08 10:53:03 +0000

    hecho eje sin gui

---
+++ src/es/etsii/ull/PAI/JuegoPremio/Main.java
@@ -0,0 +1,10 @@
+package es.etsii.ull.PAI.JuegoPremio;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Bienvenido al Juego donde todo el mundo gana");
+    JuegoPremio juego = new JuegoPremio(args[0], args[1], args[2]);
+  }
+
+}
