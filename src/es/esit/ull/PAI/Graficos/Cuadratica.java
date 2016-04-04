8f1699bce4e0e4929dcbf8caf4b28c4e7adebf0a
alu0100774054 <alu0100774054@ull.edu.es>
2016-04-04 17:39:25 +0100

    Gratificador, primera versión

--- src/es/esit/ull/PAI/Graficos/Cuadratica.java
+++ src/es/esit/ull/PAI/Graficos/Cuadratica.java
@@ -8,29 +8,32 @@ package es.esit.ull.PAI.Graficos;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class Cuadratica extends JPanel {
   private static final int SEPARA_COORDENADA = 20; // Se utiliza para separar el nombre de la coordenada
-                                                   // de la linea de la coordenada(todos los casos).
+  // de la linea de la coordenada(todos los casos).
   private final Double FACTOR_DE_ESCALA = 0.1;        // Factor de escala para distanciar los puntos.
+
+  private Double terminoA;
+  private Double terminoB;
+  private Double constante;
   
-  private int coordenadaX;
-  private int coordenadaY;
-  
-  public Cuadratica() {
-    coordenadaX = 0;
-    coordenadaY = 0;
+  public Cuadratica(Double terminoA, Double terminoB, Double constante) {
+    this.terminoA = terminoA;
+    this.terminoB = terminoB;
+    this.constante = constante;
     iniciarComponentes();
   }
-  
+
   /**
    * Método que se encarga de inicializar el componente.
    */
   private void iniciarComponentes() {
-    setBackground(Color.white);
+    setBackground(Color.black);
     setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), 
         BorderFactory.createLoweredBevelBorder()));
   }
@@ -45,44 +48,74 @@ public class Cuadratica extends JPanel {
     g.setColor(Color.red);
     g.drawLine(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
     g.drawLine(this.getWidth() / 2, 0,this.getWidth() / 2 , this.getHeight());
-    
+
     // Pintamos las etiquetas del eje.
-    g.setColor(Color.black);
+    g.setColor(Color.white);
     g.drawString("Y", (this.getWidth() / 2 ) + getSeparaCoordenada(), getSeparaCoordenada());
     g.drawString("X", (this.getWidth() - getSeparaCoordenada()), (this.getHeight() / 2 ) - getSeparaCoordenada());
-  }
-  
-  private void dibujar(Graphics g) {
-    Graphics2D g2d = (Graphics2D) g;
     
+    // Pintamos la fórmula.
+    g.setColor(Color.white);
+    String formula = "Función: " + getTerminoA() + "x^2 + " + getTerminoB() + "x + " + getConstante();
+    g.drawString(formula, getSeparaCoordenada(), getSeparaCoordenada());
+    
+    // Pintamos la función.
+    dibujarPuntos(g, getTerminoA(), getHeight(), getTerminoB(), -getHeight(), getConstante());
   }
-  
-  private double coord_x(double x)
-  {
-    double real_x = x+this.getWidth()/2;
+
+  private void dibujarPuntos(Graphics g, Double terminoA, int x2, Double terminoB, int x1, Double constante) {
+    Graphics2D g2d = (Graphics2D) g;
+    g.setColor(Color.white);
+    System.out.println("Función: " + getTerminoA() + "x^2 + " + getTerminoB() + "x + " + getConstante());
+    for (int i = x1; i < x2; i++) {
+      Double y = (Math.pow(i, 2) * getTerminoA()) + (i * getTerminoB()) + getConstante();     // El 2 es el exponente al cuadrado.
+      Double xp = (double) (i + 1);
+      Double yp = (Math.pow(xp, 2) * getTerminoA()) + (xp * getTerminoB()) + getConstante();  // El 2 es el exponente al cuadrado.
+      System.out.println("Dibujando: (" + i + ", " + y + "), (" + xp + ", " + yp + ")");
+      g.drawLine((int) coord_x(i), (int) coord_y(y), (int) coord_x(xp), (int) coord_y(yp));
+    }
+  }
+
+  private double coord_x(double x) {
+    double real_x = x + this.getWidth() / 2;
     return real_x;
   }
 
   private double coord_y(double y) {
-    double real_y = -y+this.getHeight()/2;
-    return (real_y);
+    double real_y = -y + this.getHeight() / 2;
+    return real_y;
   }
-  
-  private int getCoordenadaX() {
-    return coordenadaX;
+
+  private Double getTerminoA() {
+    return terminoA;
   }
-  private void setCoordenadaX(int coordenadaX) {
-    this.coordenadaX = coordenadaX;
+
+  private void setTerminoA(Double terminoA) {
+    this.terminoA = terminoA;
   }
-  private int getCoordenadaY() {
-    return coordenadaY;
+
+  private Double getTerminoB() {
+    return terminoB;
   }
-  private void setCoordenadaY(int coordenadaY) {
-    this.coordenadaY = coordenadaY;
+
+  private void setTerminoB(Double terminoB) {
+    this.terminoB = terminoB;
+  }
+
+  private Double getConstante() {
+    return constante;
+  }
+
+  private void setConstante(Double constante) {
+    this.constante = constante;
   }
 
   private static int getSeparaCoordenada() {
     return SEPARA_COORDENADA;
+  }
+
+  private Double getFACTOR_DE_ESCALA() {
+    return FACTOR_DE_ESCALA;
   }
   
 }
--- src/es/esit/ull/PAI/Graficos/Main.java
+++ src/es/esit/ull/PAI/Graficos/Main.java
@@ -9,23 +9,22 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Main extends JFrame {
-  private final int ANCHO = 400;  // Ancho de la ventana.
-  private final int ALTO = 400;   // Alto de la ventana.
-
+  private final int ANCHO = 800;  // Ancho de la ventana.
+  private final int ALTO = 800;   // Alto de la ventana.
   /**
    * Constructor de la clase principal.
    */
-  public Main() {
-    iniciarComponentes();
+  public Main(Double terminoA, Double terminoB, Double constante) {
+    iniciarComponentes(terminoA, terminoB, constante);
   }
   
   /**
    * Método que inicializa los componentes.
    */
-  private void iniciarComponentes() {
-    add(new Cuadratica());
+  private void iniciarComponentes(Double terminoA, Double terminoB, Double constante) {
+    add(new Cuadratica(terminoA, terminoB, constante));
 
-    setTitle("Cuadrática");
+    setTitle("Gráfica");
     setLocationRelativeTo(null);
     setSize(getANCHO(), getALTO());
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,7 +40,7 @@ public class Main extends JFrame {
   }
 
   public static void main(String[] args) {
-    Main programa = new Main();
+    Main programa = new Main(Double.parseDouble(args[0]), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
     programa.setVisible(true);
   }
 
