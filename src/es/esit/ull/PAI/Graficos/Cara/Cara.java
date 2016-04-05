fb1a43c57bac71ce89e28fa95685f4803280aedf
alu0100774054 <alu0100774054@ull.edu.es>
2016-04-05 15:06:56 +0100

    cara terminada

--- src/es/esit/ull/PAI/Graficos/Cara/Cara.java
+++ src/es/esit/ull/PAI/Graficos/Cara/Cara.java
@@ -14,8 +14,12 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class Cara extends JPanel {
-  private final int DIMENSIONES_DEL_OJO = 10; // Dimensiones del ojo.
+  private final int DIMENSIONES_DEL_OJO = 20; // Dimensiones del ojo.
   private final int SEPARACION_DEL_OJO = 30;  // Distancia del centro del ojo.
+  private final int DIMENSION_CARA = 150;     // Dimensiones del contorno de la cara.
+  private final int SEPARACION_BOCA_NORTE = 20;     // Distancia del centro de la boca.
+  private final int SEPARACION_BOCA_LATERAL = 30;     // Distancia del centro de la boca.
+  private final int SEPARACION_BOCA_SUR = 40;     // Distancia del centro de la boca.
   
   public Cara() {
     iniciarComponentes();
@@ -36,17 +40,79 @@ public class Cara extends JPanel {
   private void dibujarCara(Graphics g) {
     Graphics2D g2d = (Graphics2D) g;
     g2d.setPaint(Color.black);
-    
+    //pintarPlano(g);
+    pintarOjos(g2d);
+    pintarBoca(g2d);
+  }
+
+  private void pintarBoca(Graphics2D g2d) {
+    g2d.setColor(Color.red);
+    g2d.drawLine((this.getWidth() / 2) - getSEPARACION_BOCA_LATERAL(),
+        (this.getHeight() / 2) + getSEPARACION_BOCA_NORTE(), 
+        (this.getWidth() / 2) + getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_NORTE());
+  }
+
+  private void pintarOjos(Graphics2D g2d) {
+    g2d.setColor(Color.blue);
     // Dibujar ojo
-    g2d.fillOval((this.getWidth() / 2) - getSEPARACION_DEL_OJO(), 
+    g2d.fillOval((this.getWidth() / 2) - getSEPARACION_DEL_OJO() - (getDIMENSIONES_DEL_OJO()), 
         (this.getHeight() / 2) - getSEPARACION_DEL_OJO(), 
         getDIMENSIONES_DEL_OJO(),
         getDIMENSIONES_DEL_OJO());
     
-    g2d.fillOval((this.getHeight() / 2) + getSEPARACION_DEL_OJO(), 
+    g2d.fillOval((this.getWidth() / 2) + getSEPARACION_DEL_OJO(), 
         (this.getHeight() / 2) - getSEPARACION_DEL_OJO(), 
         getDIMENSIONES_DEL_OJO(), 
         getDIMENSIONES_DEL_OJO());
+    
+    g2d.drawOval((this.getWidth() / 2) - (getDIMENSION_CARA() / 2), 
+        (this.getHeight() / 2) - (getDIMENSION_CARA() / 2),
+        getDIMENSION_CARA(), 
+        getDIMENSION_CARA());
+  }
+
+  private void pintarPlano(Graphics g) {
+    g.setColor(Color.red);
+    // basico
+    g.drawLine(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
+    g.drawLine(this.getWidth() / 2, 0,this.getWidth() / 2 , this.getHeight());
+    // ojos
+    g.drawLine(0, 
+        (this.getHeight() / 2) - getSEPARACION_DEL_OJO(), 
+        this.getWidth(), 
+        (this.getHeight() / 2) - getSEPARACION_DEL_OJO());
+    // izq
+    g.drawLine((this.getWidth() / 2) - getSEPARACION_DEL_OJO() - (getDIMENSIONES_DEL_OJO() / 2),
+        0,
+        (this.getWidth() / 2) - getSEPARACION_DEL_OJO() - (getDIMENSIONES_DEL_OJO() / 2),
+        this.getHeight());
+    // dcha
+    g.drawLine((this.getWidth() / 2) + getSEPARACION_DEL_OJO() + (getDIMENSIONES_DEL_OJO() / 2),
+        0,
+        (this.getWidth() / 2) + getSEPARACION_DEL_OJO() + (getDIMENSIONES_DEL_OJO() / 2),
+        this.getHeight());
+    // boca
+    //norte
+    g.drawLine((this.getWidth() / 2) - getSEPARACION_BOCA_LATERAL(),
+        (this.getHeight() / 2) + getSEPARACION_BOCA_NORTE(), 
+        (this.getWidth() / 2) + getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_NORTE());
+    // sur
+    g.drawLine((this.getWidth() / 2) - getSEPARACION_BOCA_LATERAL(),
+        (this.getHeight() / 2) + getSEPARACION_BOCA_SUR(), 
+        (this.getWidth() / 2) + getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_SUR());
+    // este
+    g.drawLine((this.getWidth() / 2) - getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_NORTE(),
+        (this.getWidth() / 2) - getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_SUR());
+    // oeste
+    g.drawLine((this.getWidth() / 2) + getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_NORTE(),
+        (this.getWidth() / 2) + getSEPARACION_BOCA_LATERAL(), 
+        (this.getHeight() / 2) + getSEPARACION_BOCA_SUR());
   }
 
   private int getDIMENSIONES_DEL_OJO() {
@@ -55,6 +121,26 @@ public class Cara extends JPanel {
 
   private int getSEPARACION_DEL_OJO() {
     return SEPARACION_DEL_OJO;
+  }
+
+  private int getDIMENSION_CARA() {
+    return DIMENSION_CARA;
+  }
+
+  private int getSEPARACION_BOCA_NORTE() {
+    return SEPARACION_BOCA_NORTE;
+  }
+
+  private int getSEPARACION_BOCA_ESTE() {
+    return SEPARACION_BOCA_LATERAL;
+  }
+
+  private int getSEPARACION_BOCA_SUR() {
+    return SEPARACION_BOCA_SUR;
+  }
+
+  private int getSEPARACION_BOCA_LATERAL() {
+    return SEPARACION_BOCA_LATERAL;
   }
   
 }
